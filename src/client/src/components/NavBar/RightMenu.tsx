74931b7fa2d5a77e028bb391d9ab89319d9f2f11
cybattis <cybattis@student.42lyon.fr>
2023-04-22 10:27:49 +0200

    update (UI): prettier refactor navBar right menu and home.

--- src/client/src/components/NavBar/RightMenu.tsx
+++ src/client/src/components/NavBar/RightMenu.tsx
@@ -1,23 +1,27 @@
 import React from "react";
 import "./RightMenu.css";
 import logo from "../../resource/signin-logo.svg";
-import {AuthProps} from "../../pages/Home/Home";
+import { AuthProps } from "../../pages/Home/Home";
 
-export default function RightMenu(props: AuthProps ) {
+export default function RightMenu(props: AuthProps) {
   return (
     <div className="rightMenu">
-      <button className="login-button" onClick={
-          () => {
-              props.loginCallback(true);
-              props.signupCallback(false);
-          }}>
+      <button
+        className="login-button"
+        onClick={() => {
+          props.loginCallback(true);
+          props.signupCallback(false);
+        }}
+      >
         Login
       </button>
-      <button className="signup-button" onClick={
-          () => {
-              props.signupCallback(true);
-              props.loginCallback(false);
-          }}>
+      <button
+        className="signup-button"
+        onClick={() => {
+          props.signupCallback(true);
+          props.loginCallback(false);
+        }}
+      >
         <img className="logo-signin" src={logo} alt="logo" />
         SignUp
       </button>
--- src/client/src/pages/Home/Home.tsx
+++ src/client/src/pages/Home/Home.tsx
@@ -1,35 +1,36 @@
 import NavBar from "../../components/NavBar/NavBar";
 import Footer from "../../components/Footer/Footer";
 import "./Home.css";
-import {useState} from "react";
+import { useState } from "react";
 import Login from "../Login/Login";
 import Signup from "../Signup";
 
 export interface AuthProps {
-    loginCallback: (value: any) => void,
-    signupCallback: (value: any) => void
+  loginCallback: (value: any) => void;
+  signupCallback: (value: any) => void;
 }
 
 function HomePage() {
-    return (
+  return (
     <div className="home">
-        <h1>PongFever</h1>
-        <h3 className="desc">
-            Come play to the new and fun multiplayer pong game !
-        </h3>
-        <div className="game"></div>
+      <h1>PongFever</h1>
+      <h3 className="desc">
+        Come play to the new and fun multiplayer pong game !
+      </h3>
+      <div className="game"></div>
     </div>
-    );
+  );
 }
 
 export default function Home() {
-    const [loginState, setLoginState] = useState(false);
-    const [signupState, setSignupState] = useState(false);
+  const [loginState, setLoginState] = useState(false);
+  const [signupState, setSignupState] = useState(false);
 
   return (
     <div className="full">
       <NavBar loginCallback={setLoginState} signupCallback={setSignupState} />
-      {(loginState) ? <Login /> : (signupState) ? <Signup /> : <HomePage />}
+      {loginState ? <Login /> : signupState ? <Signup /> : null}
+      <HomePage />
       <Footer />
     </div>
   );
