a1706cf398d6c4e36f010e5239e1c68b74680e5c
cybattis <cybattis@student.42lyon.fr>
2023-05-12 11:42:52 +0200

    update (API/user): add Get(:login) entry point and findById function in user module.
      - Add creationDate, updateDate, avatarUrl, xp field

--- src/api/src/user/user.controller.ts
+++ src/api/src/user/user.controller.ts
@@ -12,6 +12,22 @@ export class UserController {
     return this.userService.findAll();
   }
 
+  @Get(':login')
+  async userInfo(@Param('login') login: string): Promise<any> {
+    const user = await this.userService.findByLogin(login);
+
+    if (!user) return null;
+    return {
+      id: user.id,
+      nickname: user.nickname,
+      // last 10 matches
+      // Stats
+      // Channel list ?
+      // Friends list ?
+      // Achievements ?
+    };
+  }
+
   @Get('check/login/:input')
   async checkNicknameInUse(@Param('input') input: string) {
     return this.userService.findByLogin(input);
