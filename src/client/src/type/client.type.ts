58468e2474871e4efec97b8a3e2780b988ce953e
cybattis <cybattis@student.42lyon.fr>
2023-05-29 12:21:00 +0200

    feat (Types): remove unused shared types.

--- src/client/src/type/client.type.ts
+++ src/client/src/type/client.type.ts
@@ -1,12 +1,3 @@
 export interface Decoded {
   id: string;
 }
-
-export interface LeaderboardItemProps {
-  rank: number;
-  avatar: string;
-  nickname: string;
-  winrate: number;
-  gamePlayed: number;
-  elo: number;
-}
