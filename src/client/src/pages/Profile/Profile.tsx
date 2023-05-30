bbb94755c73d8a60ff39c1132d6b02a7dd25bcc1
cybattis <cybattis@student.42lyon.fr>
2023-05-30 12:43:56 +0200

    update (Client): update profile page style.

--- src/client/src/pages/Profile/Profile.tsx
+++ src/client/src/pages/Profile/Profile.tsx
@@ -1,28 +1,45 @@
+import "./Profile.css";
 import { useLoaderData } from "react-router-dom";
 import { UserInfo } from "../../type/user.type";
+import { Avatar } from "../../components/Avatar";
+import { XPBar } from "../../components/XPBar/XPBar";
 
 export function Profile() {
   let data = useLoaderData() as UserInfo;
+  const winrate = 0;
 
   console.log(data);
 
-  const style = {
-    display: "flex",
-    flexDirection: "column" as "column",
-    alignItems: "center",
-    justifyContent: "center",
-    height: "var(--page-height)",
-  };
-
   return (
-    <div style={style}>
-      <div>Profile</div>
-      <div>nickname: {data.nickname}</div>
-      <div>level: {data.level}</div>
-      <div>xp: {data.xp}</div>
-      <div>ranking: {data.ranking}</div>
-      <div>Game played: {data.games?.length}</div>
-      <div>Winrate: 0%</div>
+    <div className={"profilePage"}>
+      <div id={"infobox"}>
+        <Avatar size="200px" img={data.avatarUrl} />
+        <div id="info">
+          <h1 id={"nickname"}>{data.nickname}</h1>
+          <div>LVL {data.level}</div>
+          <p>{data.xp} xp</p>
+          <XPBar xp={data.xp} lvl={data.level} />
+        </div>
+      </div>
+      <div id={"stats"}>
+        <div id={"elo"} className={"dataBox"}>
+          <div>ELO</div>
+          <div>{data.ranking}</div>
+        </div>
+        <div id={"gamePlayed"} className={"dataBox"}>
+          <div>Game played</div>
+          <div>{data.games?.length}</div>
+        </div>
+        <div id={"winrate"} className={"dataBox"}>
+          <div>Winrate</div>
+          <div>{winrate}%</div>
+        </div>
+      </div>
+      <div id={"gamesStatsBox"}>
+        <h3 id={"gamesStatsBoxTitle"}>Games</h3>
+        <hr id={"hrbar"} />
+        <div id={"gamesStats"}></div>
+      </div>
     </div>
   );
 }
