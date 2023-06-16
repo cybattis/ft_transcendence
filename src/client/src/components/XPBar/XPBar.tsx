e0fd5043f4b19f188c65bf96afe45a7b30b80d49
cybattis <cybattis@student.42lyon.fr>
2023-06-16 16:24:03 +0200

    fix (client): fix xpBar

--- src/client/src/components/XPBar/XPBar.tsx
+++ src/client/src/components/XPBar/XPBar.tsx
@@ -1,8 +1,6 @@
 import "./XPBar.css";
 
 export function XPBar(props: { xp: number; lvl: number }) {
-    console.log("XP: ", props.xp);
-    console.log("Lvl: ", props.lvl);
   return (
     <>
       <progress id="XPBar" max={1000} value={props.xp}></progress>
