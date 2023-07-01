94175dd14db982536a5d05f53001b9eb946c3368
cybattis <cybattis@student.42lyon.fr>
2023-07-01 22:11:41 +0200

    WIP (Client): WIP about page.

--- src/client/src/pages/About/About.tsx
+++ src/client/src/pages/About/About.tsx
@@ -1,4 +1,17 @@
+import { AuthContext } from "../../components/Auth/dto";
+import { useContext } from "react";
+import { ErrorContext } from "../../components/Modal/modalContext";
+
 export default function About() {
+  const token = localStorage.getItem("token");
+  const { setAuthToken } = useContext(AuthContext);
+  const { setErrorMessage } = useContext(ErrorContext);
+
+  if (token === null && setAuthToken !== null) {
+    setAuthToken(null);
+    setErrorMessage("Session expired, please login again!");
+  }
+
   return (
     <div>
       <h1>Team !</h1>
