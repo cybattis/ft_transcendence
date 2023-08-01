939a22c53b9ab2e4e0f26f7e743d2a614a3ff49c
Nathan Jennes <nathanjsim42@gmail.com>
2023-08-01 14:53:39 +0200

    Authed Route: implemented Authed route

--- src/client/src/components/Auth/AuthedRoute.tsx
+++ src/client/src/components/Auth/AuthedRoute.tsx
@@ -1,47 +1,14 @@
-import {ReactElement, ReactNode, useContext, useEffect, useState} from "react";
+import {ReactElement, useContext} from "react";
 import { AuthContext } from "./auth.context";
 import { Navigate } from "react-router-dom";
-import Loading from "react-loading";
-import {ErrorContext} from "../Modal/modalContext";
-import axios from "axios";
-import {apiBaseURL} from "../../utils/constant";
+import {LoadingPage} from "../../pages/Loading/LoadingPage";
 
 interface AuthedRouteProps {
   component: ReactElement,
 }
 
 export function AuthedRoute({component}: AuthedRouteProps) {
-  const { authed, setAuthed } = useContext(AuthContext);
-  const { setErrorMessage } = useContext(ErrorContext);
-  const [ loading, setLoading ] = useState<boolean>(true);
+  const { authed, isAuthing } = useContext(AuthContext);
 
-  useEffect(() => {
-    if (authed) return;
-
-    const token = localStorage.getItem("token");
-    if (!token) {
-      setLoading(false);
-      setAuthed(false);
-      setErrorMessage("Session expired, please login again!");
-      return;
-    }
-
-    axios
-      .get(apiBaseURL + "auth/token-validation", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        }
-      }).then((res) => {
-      setAuthed(true);
-      console.log("token valid");
-    }).catch((error) => {
-      setAuthed(false);
-      console.log("token invalid");
-      setErrorMessage("Your session has expired, please log in again.");
-      localStorage.removeItem("token");
-      return <Navigate to="/"/>;
-    });
-  }, []);
-
-  return authed ? component : ( loading ? <Loading/> : <Navigate to={"/"}/>);
+  return authed ? component : ( isAuthing ? <LoadingPage/> : <Navigate to={"/"}/>);
 }
