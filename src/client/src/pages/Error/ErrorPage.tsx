2ea100566dc946f48f85aaa981d3d23ca1827a86
nathan <nathanjsim42@gmail.com>
2023-08-10 01:40:55 +0200

    Error Page: Fix css

--- src/client/src/pages/Error/ErrorPage.tsx
+++ src/client/src/pages/Error/ErrorPage.tsx
@@ -1,23 +1,33 @@
+import "./ErrorPage.css"
+
 export function Error400Page() {
   return (
-    <h1 className="errorPage">Sorry, we can't show that right now 🫣</h1>
+    <div className="errorPage">
+      <h1>Sorry, we can't show that right now 🫣</h1>
+    </div>
   );
 }
 
 export function Error404Page() {
   return (
-    <h1 className="errorPage">Sorry, we didn't find what you wanted...🧐</h1>
+    <div className="errorPage">
+      <h1>Sorry, we didn't find what you wanted...🧐</h1>
+    </div>
   );
 }
 
 export function Error500Page() {
   return (
-    <h1 className="errorPage">Sorry, we are having some technical issues...🔧</h1>
+    <div className="errorPage">
+      <h1>Sorry, we are having some technical issues...🔧</h1>
+    </div>
   );
 }
 
 export function ErrorInternetPage() {
   return (
-    <h1 className="errorPage">Sorry, you seem to have internet issues...📡</h1>
+    <div className="errorPage">
+      <h1>Sorry, you seem to have internet issues...📡</h1>
+    </div>
   );
 }
