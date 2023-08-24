d5a64b02687999f986e9e40f00073ddc3833f101
nathan <nathanjsim42@gmail.com>
2023-08-24 17:52:38 +0200

    Profile Page: Same fix as in Home Page

--- src/client/src/components/Game/MatcheScore.tsx
+++ src/client/src/components/Game/MatcheScore.tsx
@@ -21,17 +21,23 @@ export function MatcheScore(props: { game: GameStats; userId: number }) {
   const state = () => {
     if (props.game.status === GameStatus.IN_PROGRESS)
       return <div id={"md-in-progress"}>In progress</div>;
-    else if (
-      (props.game.ids[0] === props.userId && props.game.status !== GameStatus.PLAYER1_DISCONNECTED &&
-        props.game.scoreP1 > props.game.scoreP2 ||
-      (props.game.ids[0] === props.userId &&
-        props.game.status === GameStatus.PLAYER2_DISCONNECTED)) ||
-      (props.game.ids[1] === props.userId && props.game.status !== GameStatus.PLAYER2_DISCONNECTED &&
-        props.game.scoreP1 < props.game.scoreP2 ||
-      (props.game.ids[1] === props.userId && props.game.status === GameStatus.PLAYER1_DISCONNECTED))
-    ) {
-      return <div id={"md-win"}>{score()}</div>;
-    } else return <div id={"md-loose"}>{score()}</div>;
+    else {
+      const isPlayer1 = props.game.ids[0] === props.userId;
+      const isPlayer2 = props.game.ids[1] === props.userId;
+
+      const player1Won = (props.game.scoreP1 > props.game.scoreP2 && props.game.status !== GameStatus.PLAYER1_DISCONNECTED)
+        || props.game.status === GameStatus.PLAYER2_DISCONNECTED;
+
+      const player2Won = (props.game.scoreP1 < props.game.scoreP2 && props.game.status !== GameStatus.PLAYER2_DISCONNECTED)
+        || props.game.status === GameStatus.PLAYER1_DISCONNECTED;
+
+      const isWin = (isPlayer1 && player1Won) || (isPlayer2 && player2Won);
+
+      if (isWin)
+        return <div id={"md-win"}>{score()}</div>;
+      else
+       return <div id={"md-loose"}>{score()}</div>;
+    }
   };
 
   return <div id={"md-score"}>{state()}</div>;
